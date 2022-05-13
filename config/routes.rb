1119d3f6dc848198b0e27310f11ef4167df5d1ab
Sanosuke95 <cedric-valcy@hotmail.fr>
2022-05-13 22:31:21 +0200

    Ajout des pages statiques

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
